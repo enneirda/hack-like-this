c64e2e99bcfd4e72d4396438e94af300829508e8
Adrienne Bailey <amb1248@nyu.edu>
2024-02-09 22:39:20 -0500

    things

---
+++ app/components/YTVid.tsx
@@ -0,0 +1,21 @@
+'use client'
+import React from 'react';
+import YouTube, { YouTubeProps } from 'react-youtube';
+
+export function YoutubeVideo() {
+  const onPlayerReady: YouTubeProps['onReady'] = (event) => {
+    // access to player in all event handlers via event.target
+    event.target.pauseVideo();
+  }
+
+  const opts: YouTubeProps['opts'] = {
+    height: '390',
+    width: '640',
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
+  return <YouTube videoId="zgtfEEuqLEw" opts={opts} onReady={onPlayerReady} />;
+}
